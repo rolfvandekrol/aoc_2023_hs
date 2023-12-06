f791b348cca9a8184cc549f48cb1d6b636905e3d
Rolf van de Krol <rvandekrol@swis.nl>
2023-12-06 16:21:51 +0100

    day 1

---
+++ 01/puzzle1.hs
@@ -0,0 +1,42 @@
+import qualified Data.Map as Map
+import qualified Data.List as List
+
+m :: [(String, Int)]
+m = [("1", 1),
+     ("2", 2),
+     ("3", 3),
+     ("4", 4),
+     ("5", 5),
+     ("6", 6),
+     ("7", 7),
+     ("8", 8),
+     ("9", 9)]
+
+findDigitPrefix :: String -> Maybe Int
+findDigitPrefix x = case List.find (\(prefix, value) -> prefix `List.isPrefixOf` x) m of
+    Just (_, value) -> Just value
+    Nothing -> Nothing
+
+findDigitSuffix :: String -> Maybe Int
+findDigitSuffix x = case List.find (\(prefix, value) -> prefix `List.isSuffixOf` x) m of
+    Just (_, value) -> Just value
+    Nothing -> Nothing
+
+firstDigit :: String -> Int
+firstDigit "" = error "not found"
+firstDigit o = case findDigitPrefix o of
+    Just d -> d
+    Nothing -> firstDigit (tail o)
+
+lastDigit :: String -> Int
+lastDigit "" = error "not found"
+lastDigit o = case findDigitSuffix o of
+    Just d -> d
+    Nothing -> lastDigit (init o)
+
+solve :: String -> Int
+solve v = sum [ 10 * firstDigit l + lastDigit l | l <- lines v]
+
+main = do
+    input <- getContents
+    print (solve input)
